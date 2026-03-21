005ffc9a3dd6d6f41d6af79b937a4c01abdd81f5
Tomáš Gallischek <t.gall@seznam.cz>
2026-03-21 07:15:52 +0100

    Automaticke ulozeni: 2026-03-21 07:15:52

--- profile_app/models.py
+++ profile_app/models.py
@@ -1,10 +1,51 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+
 
 class Player_info(models.Model):
+    
+# ZÁKLADNÍ INFORMACE
     username = models.OneToOneField('auth.User', on_delete=models.CASCADE)
-    level = models.IntegerField(default=1)
-    xp = models.IntegerField(default=0)
+    lvl = models.IntegerField(default=1, validators=[MinValueValidator(1)])
+    xp = models.IntegerField(default=0, validators=[MinValueValidator(0)])
+    xp_next_lvl = models.IntegerField(default=100, validators=[MinValueValidator(1)])
+    gold = models.IntegerField(default=0, validators=[MinValueValidator(0)])
     
+# KROKY
+    steps = models.IntegerField(default=0, validators=[MinValueValidator(0)])
+    steps_today = models.IntegerField(default=0, validators=[MinValueValidator(0)])
     
+# ATRIBUTY
+    str_base = models.IntegerField(default=1, blank=True)
+    str_stats = models.IntegerField(default=0, blank=True)
+    str_eqp = models.IntegerField(default=0, blank=True)
+    str_max = models.IntegerField(default=1, blank=True)
+    
+    dex_base = models.IntegerField(default=1, blank=True)
+    dex_stats = models.IntegerField(default=0, blank=True)
+    dex_eqp = models.IntegerField(default=0, blank=True)
+    dex_max = models.IntegerField(default=1, blank=True)
+    
+    int_base = models.IntegerField(default=1, blank=True)
+    int_stats = models.IntegerField(default=0, blank=True)
+    int_eqp = models.IntegerField(default=0, blank=True)
+    int_max = models.IntegerField(default=1, blank=True)    
+
+    vit_base = models.IntegerField(default=1, blank=True)
+    vit_stats = models.IntegerField(default=0, blank=True)
+    vit_eqp = models.IntegerField(default=0, blank=True)
+    vit_max = models.IntegerField(default=1, blank=True)
+    
+    luck_base= models.IntegerField(default=1, blank=True)
+    luck_stats = models.IntegerField(default=0, blank=True)
+    luck_eqp = models.IntegerField(default=0, blank=True)
+    luck_max = models.IntegerField(default=1, blank=True)
+    
+# BOJ
+    hp = models.IntegerField(default=50, blank=True)
+    attack_number = models.IntegerField(default=1, blank=True)
+    attack_speed = models.FloatField(default=1, blank=True)
+    defence_number = models.IntegerField(default=1, blank=True)    
+
     def __str__(self):
-        return self.username.username
+        return f"{self.username} - lvl: {self.lvl} - xp: {self.xp} - gold: {self.gold}"
